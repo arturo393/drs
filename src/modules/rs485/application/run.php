5a565a26981b0bb270053a7c859c72f3d1f2cf43
Guillermo Gonzalez <ggonzalez@itaum.com>
2022-03-02 16:28:07 -0300

    se agrega libreria

---
+++ src/modules/rs485/application/run.php
@@ -0,0 +1,11 @@
+<?php
+// Icinga Rs485 | (c) 2018 Icinga GmbH | GPLv2
+
+namespace Icinga\Module\Rs485 {
+
+    use Icinga\Application\Icinga;
+
+    /** @var \Icinga\Application\Modules\Module $this */
+
+    //Icinga::app()->getLoader()->registerNamespace('reportingipl\Html', __DIR__ . '/library/vendor/ipl/Html/src');
+}
