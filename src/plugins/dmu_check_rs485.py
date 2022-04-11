073973bcbe57af81bbdf0844f82dccde2326419c
sigmadev2 <a.veras@gmail.com>
2022-04-11 12:07:22 -0400

    +plugin: fix alarm display

--- src/plugins/dmu_check_rs485.py
+++ src/plugins/dmu_check_rs485.py
@@ -557,15 +557,17 @@ def main():
     
     alarm =""
     exit_value = 0
-    if dlPower >= high_level_warning_downlink:
-        alarm ="<h3><font color=\"#ffaa44\">Downlink Power Level Warning "+ parameter_dict['ulInputPower']+ "[dBm]</font></h3>"
-    if ulPower >= high_level_warning_uplink:
-        alarm ="<h3><font color=\"#ffaa44\">Uplink Power Level Warning " +parameter_dict['dlOutputPower']+"[dBm]</font></h3>"
-    if ulPower >= high_level_critical_uplink:
-
-        alarm ="<h3><font color=\"#ff5566\">Uplink Power Level Critical " +parameter_dict['dlOutputPower']+"[dBm]!</font></h3>"  
     if dlPower >= high_level_critical_downlink:
-        alarm ="<h3><font color=\"#ff5566\">Downlink Power Level Critical "+ parameter_dict['ulInputPower']+ " [dBn]!</font></h3>"
+        alarm +="<h3><font color=\"#ff5566\">Downlink Power Level Critical "+ parameter_dict['ulInputPower']+ " [dBn]!</font></h3>"
+    elif dlPower >= high_level_warning_downlink:
+        alarm +="<h3><font color=\"#ffaa44\">Downlink Power Level Warning "+ parameter_dict['ulInputPower']+ "[dBm]</font></h3>"
+
+    if ulPower >= high_level_critical_uplink:
+        alarm +="<h3><font color=\"#ff5566\">Uplink Power Level Critical " +parameter_dict['dlOutputPower']+"[dBm]!</font></h3>"      
+    elif ulPower >= high_level_warning_uplink:
+        alarm +="<h3><font color=\"#ffaa44\">Uplink Power Level Warning " +parameter_dict['dlOutputPower']+"[dBm]</font></h3>"
+ 
+
             
             
     parameter_html_table = create_table(parameter_dict)  
