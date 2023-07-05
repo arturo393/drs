21d478680fc6b5c1b7b7b5ffc59545b075b97c87
Arturo Veras <arturo@sigma-telecom.com>
2023-07-05 08:04:06 -0400

    add get_host support from wsl machine

--- master-installer/get_hosts.sh
+++ master-installer/get_hosts.sh
@@ -5,7 +5,6 @@ yaml_file=$1
 
 # Extract the children of "hosts"
 children=$(grep -A9999 "hosts:" "$yaml_file" | awk '/^[[:blank:]]+[[:alnum:]_.-]+:/{gsub(/:/, "", $1); print $1}' | sed '/hosts/d')
-
-# Print the children
+#children=$(grep -E "^[[:space:]]+[0-9]" "$yaml_file" | cut -d':' -f1 | sed 's/^[[:space:]]*//')
 echo "$children"
 
