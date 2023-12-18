d2110abc81f6706479441892cc53e3e4ca366815
arturo33 <arturo@sigma-telecom.com>
2023-12-18 11:15:30 -0300

    [FIX] master serial set parameters

--- src/modules/rs485/application/forms/HostForm.php
+++ src/modules/rs485/application/forms/HostForm.php
@@ -10,17 +10,6 @@ class HostForm extends ConfigForm
 {
     use Database;
 
-    private $listComando = [
-
-        1, # 'Working mode','Set', '7E', '07','00','00','80','00', '01', '02','0fD7', '7E');
-        2, #'Gain power control ATT','Set','7E','07','00','00','E7','00','02','0034','7783','7E');
-        3, #'Channel Activation Status','Set','7E','07','00','00','41','00','10','010101011101010101010101010101','11BD','7E');
-        4, #'Channel Frequency Point Configuration','Set','7E','07','00','00','35','00','40','B02741002D28410027294100A4294100212A41009E2A41001B2B4100982B4100152C4100922C41000F2D41008C2D4100092E4100862E4100032F4100802F4100','7783','7E');
-        5, #'Optical PortState','Set','7E','07','00','00','90','00','04','00010001','9532','7E');
-
-
-    ];
-
     public function init()
     {
         $this->setName('form_host');
@@ -28,11 +17,11 @@ class HostForm extends ConfigForm
         $this->setAction('rs485/host/edit');
     }
 
-    public function refresh(string $host, int $centerUplinkFrequency, int $centerDownlinkFrequency, int $bandwidth, string $cmd_type, string $device): void
+    public function refresh(string $host, float $centerUplinkFrequency, float $centerDownlinkFrequency, int $bandwidth, string $cmd_type, string $device): void
     {
         $this->setName('form_host');
         $this->setSubmitLabel($this->translate('Submit Changes'));
-        $this->setAction(sprintf('rs485/host/edit?host=%s&center_uplink_frequency=%d&center_downlink_frequency=%d&bandwidth=%d&cmd_type=%s&device=%s', $host, $centerUplinkFrequency, $centerDownlinkFrequency, $bandwidth, $cmd_type, $device));
+        $this->setAction(sprintf('rs485/host/edit?host=%s&center_uplink_frequency=%f&center_downlink_frequency=%f&bandwidth=%d&cmd_type=%s&device=%s', $host, $centerUplinkFrequency, $centerDownlinkFrequency, $bandwidth, $cmd_type, $device));
     }
 
     public function createElements(array $formData)
@@ -43,9 +32,9 @@ class HostForm extends ConfigForm
         $center_uplink_frequency = $_GET['center_uplink_frequency'] ?? 0;
         $center_downlink_frequency = $_GET['center_downlink_frequency'] ?? 0;
         $bandwidth = $_GET['bandwidth'] ?? 0;
-        $cmd_type = $_GET['cmd_type'] ?? "";
-        $device = $_GET['device'] ?? "";
-        $host = $_GET['host'] ?? "";
+        $host = $_GET['host'] ?? 0;
+        $cmd_type = $_GET['host'] ?? "";
+        $device = $_GET['host'] ?? "";
         $this->refresh($host, $center_uplink_frequency, $center_downlink_frequency, $bandwidth, $cmd_type, $device);
 
         $this->addElement('select', 'host_remote', array(
@@ -62,6 +51,7 @@ class HostForm extends ConfigForm
 
         if ((isset($formData['trama']) && $formData['trama'] != '') || isset($formData['btn_submit'])) {
             $option = isset($formData['btn_submit']) ? 0 : $formData['trama'];
+
             #5: Optical PortState
             if ($option == 5 || $option == 999 || isset($formData['opt5_hidden'])) {
                 $input = 5;
@@ -69,7 +59,7 @@ class HostForm extends ConfigForm
                 $hidden = isset($formData["opt{$input}_hidden"]) ? $formData["opt{$input}_hidden"] : 0;
                 if ($option != $hidden) {
                     $this->addElement('hidden', "opt{$input}_hidden", ['value' => $input]);
-                    $descripcion = $this->getDescripcion($input);
+
                     $this->addElement('checkbox', "opt{$input}_1", ['label' => "Enable - Port 1"]);
 
                     $this->addElement('checkbox', "opt{$input}_2", ['label' => "Enable - Port 2"]);
@@ -100,7 +90,6 @@ class HostForm extends ConfigForm
                 $hidden = isset($formData["opt{$input}_hidden"]) ? $formData["opt{$input}_hidden"] : 0;
                 if ($option != $hidden) {
                     $this->addElement('hidden', "opt{$input}_hidden", ['value' => $input]);
-                    $descripcion = $this->getDescripcion($input);
                     $this->addElement('text', "opt{$input}_1", ['label' => "Uplink ATT [dB]", 'placeholder' => 'between 0[dB] and 30[dB]', 'required' => true,]);
                     $this->addElement('text', "opt{$input}_2", ['label' => 'Downlink ATT [dB]', 'placeholder' => 'between 0[dB] and 30[dB]', 'required' => true,]);
                 }
@@ -111,7 +100,6 @@ class HostForm extends ConfigForm
                 $hidden = isset($formData["opt{$input}_hidden"]) ? $formData["opt{$input}_hidden"] : 0;
                 if ($option != $hidden) {
                     $this->addElement('hidden', "opt{$input}_hidden", ['value' => $input]);
-                    $descripcion = $this->getDescripcion($input);
                     for ($i = 1; $i <= 16; $i++) {
                         $this->addElement('radio', "opt{$input}_{$i}", array(
                             'label' => $this->translate("Channel {$i}"),
@@ -200,8 +188,15 @@ class HostForm extends ConfigForm
         if ($center_downlink_frequency == 0 or $bandwidth == 0 or $center_uplink_frequency == 0) {
             $list = ["Unknown Frequencies"];
         } else {
-            $ul = (float)$center_uplink_frequency - (int)($bandwidth) / 2;
-            $dl = (float)$center_downlink_frequency - (int)($bandwidth) / 2;
+            $half_bandwidth = ($bandwidth / 2.0);
+            $ul = (float)$center_uplink_frequency - ($bandwidth / 2.0);
+            $dl = (float)$center_downlink_frequency - ($bandwidth / 2.0);
+            print($center_downlink_frequency . "\n");
+            print($center_uplink_frequency . "\n");
+            print($half_bandwidth . "\n");
+            print($bandwidth . "\n");
+            print($ul . "\n");
+            print($dl . "\n");
             $wb = $bandwidth;
             $cb = 125; // 12.5 Khz
             $ch_number_length = 4;
