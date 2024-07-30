ad3693abe071c2ed10c766ac2ecd1fe9fc8dea3f
arturo33 <arturo@sigma-telecom.com>
2024-07-30 14:26:23 -0400

    [ADD]  parameter class to handle decoded data

--- src/plugins/drs/parameters.py
+++ src/plugins/drs/parameters.py
@@ -1,3 +1,5 @@
+
+
 from src.plugins.drs.dru import DRU
 
 
@@ -37,11 +39,14 @@ class Parameters:
             dru_connected[f"opt{optical_port}"] = []
             for connected in range(1, int(devices_connected) + 1):
                 device_id = self._get_device_id(self.parameters, optical_port, connected)
-                if device_id == 0:
-                    continue
+
 
                 ip = self._generate_ip(self.net_prefix, optical_port, connected)
                 parent = self._get_parent_name(self.hostname, dru_connected, optical_port, connected)
+
+                if device_id == 0:
+                    # TODO: agregar consulta de id al remoto
+                    continue
 
                 dru = DRU(connected, optical_port, device_id, self.hostname, ip, parent)
                 dru_connected[f"opt{optical_port}"].append(dru)
