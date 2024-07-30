e68df7199a35450c1141ee827f9a5c097b7b393e
arturo33 <arturo@sigma-telecom.com>
2024-07-30 14:25:25 -0400

    [ADD]  command executor

---
+++ src/plugins/drs/command_executor.py
@@ -0,0 +1,34 @@
+import sys
+
+from src.plugins.drs.definitions.nagios import WARNING, CRITICAL, OK, UNKNOWN
+from src.plugins.drs.command import Command
+
+
+class CommandExecutor:
+    def __init__(self, args):
+        self.args = args
+        self.command = Command(args=self.args)
+
+    def execute(self, cmd_type):
+        # Create command
+        exit_code, message = self.command.create_command(cmd_type)
+        self._check_critical(exit_code, message)
+
+        # Transmit and receive
+        exit_code, message = self.command.transmit_and_receive()
+        self._check_critical(exit_code, message)
+
+        # Extract and decode
+        if not self.command.extract_and_decode_received():
+            self._exit_critical("No decoded data")
+
+    def get_command(self):
+        return self.command
+
+    def _check_critical(self, exit_code, message):
+        if exit_code == CRITICAL:
+            self._exit_critical(message)
+
+    def _exit_critical(self, message):
+        sys.stderr.write(f"CRITICAL - {message}")
+        sys.exit(CRITICAL)
